153ea01633ab6b56f2f2b9fab465a27ec51ce5c7
gombi <gombi@>
2021-05-29 22:09:46 -0400

    Fix

--- bin/infra-init.sh
+++ bin/infra-init.sh
@@ -197,15 +197,15 @@ if ! [ "$HOST" == "vm" ] && ! [ "$HOST" == "pincer" ] ; then
   echo "LABEL=swap none swap nofail,x-systemd.device-timeout=5 0 0" >> $R/etc/fstab
 fi
 
-if [ -d dnsmasq ]; then
+if [ -f dhcp.conf ]; then
   # DHCP
-  cp dnsmasq/dhcp.conf $R/etc/dnsmasq.d/
+  cp dhcp.conf $R/etc/dnsmasq.d/
   chmod 444 $R/etc/dnsmasq.d/dhcp.conf
 
   echo "127.0.0.1 localhost" > $R/etc/hosts
   chmod 444 $R/etc/hosts
 
-  cat dnsmasq/dhcp.conf | grep ^dhcp-host | awk 'BEGIN { FS = "," } ; { print $3 " " $2}' >> $R/etc/hosts
+  cat dhcp.conf | grep ^dhcp-host | awk 'BEGIN { FS = "," } ; { print $3 " " $2}' >> $R/etc/hosts
   ln -sf /lib/systemd/system/dnsmasq.service $R/etc/systemd/system/multi-user.target.wants/dnsmasq.service
 fi
 
