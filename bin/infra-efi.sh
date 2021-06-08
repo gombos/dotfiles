79b2d027fc005c540adcf43059a8082bba970ca1
gombi <gombi@>
2021-06-08 00:56:31 -0400

    Fix export

--- bin/infra-efi.sh
+++ bin/infra-efi.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-container_id=$(sudo docker run -v /tmp:/tmp 0gombi0/homelab:efi rsync -av /efi /tmp)
+container_id=$(sudo docker create -ti -v /tmp:/tmp 0gombi0/homelab:efi bash)
+sudo docker start $container_id
 
-sudo docker rm $container_id
+sudo docker exec $container_id  rsync -av /efi /tmp
+
+sudo docker rm -f $container_id
 
