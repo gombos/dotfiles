88ea85230e9477f916014be29e023ad0164905b1
gombi <gombi@>
2021-06-12 15:55:49 -0400

    Some notes

--- bin/infra-partition-liveimage.sh
+++ bin/infra-partition-liveimage.sh
@@ -9,7 +9,7 @@ gpt destroy $DISK
 
 gpt create -f $DISK
 
-# EFI (fat32) - 200 MB
+# EFI (fat32) - 1 GB
 gpt add -i 1 -b        40 -s   2095064 -t C12A7328-F81F-11D2-BA4B-00A0C93EC93B $DISK
 
 # Linux (btrfs)
@@ -23,6 +23,8 @@ gpt add -i 4 -b  118833000 -t 48465300-0000-11AA-AA11-00306543ECAC $DISK
 
 diskutil umountDisk $DISK
 
-MS data
+#MS data
 # EBD0A0A2-B9E5-4433-87C0-68B6B72699C7
-https://en.wikipedia.org/wiki/GUID_Partition_Table
+#https://en.wikipedia.org/wiki/GUID_Partition_Table
+# diskutil list
+# gpt -r show
