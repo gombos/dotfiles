2ad42d680a554f25ea916876b0e683e7ec06bd8b
gombi <gombi@>
2021-07-04 20:42:39 -0400

    Fix spacing

--- bin/infra-vm-image.sh
+++ bin/infra-vm-image.sh
@@ -18,64 +18,65 @@ MNT_DIR=${MNT_DIR:=$OUT_DIR/mnt}
 MNT=$MNT_DIR/root
 MNT_EFI=$MNT_DIR/efi
 
-  mkdir -p $MNT $MNT_EFI
-  echo "Installing $RELEASE into $FILE..."
+mkdir -p $MNT $MNT_EFI
+echo "Installing $RELEASE into $FILE..."
 
-  # 3GB image file to fit comfortable to 8 GB sticks or larger
-  IMGSIZE=${IMGSIZE:=3072} # in megabytes
+# 3GB image file to fit comfortable to 8 GB sticks or larger
+IMGSIZE=${IMGSIZE:=3072} # in megabytes
 
-  if [ -f $FILE ]; then
-    sudo rm -rf $FILE
+if [ -f $FILE ]; then
+  sudo rm -rf $FILE
+fi
+
+if [ ! -f $FILE ]; then
+  echo "Creating $FILE"
+  sudo dd if=/dev/zero of=$FILE bs=1024k seek=${IMGSIZE} count=0
+fi
+
+# Assigning loopback device to the image file
+DISK=""
+for i in /dev/loop* # or /dev/nbd*
+do
+  if sudo losetup $i $FILE
+  then
+    DISK=$i
+    break
   fi
+done
+[ "$DISK" == "" ] && fail "no loop device available"
 
-  if [ ! -f $FILE ]; then
-    echo "Creating $FILE"
-    sudo dd if=/dev/zero of=$FILE bs=1024k seek=${IMGSIZE} count=0
-  fi
+echo "Connected $FILE to $DISK"
 
-  # Assigning loopback device to the image file
-  DISK=""
-  for i in /dev/loop* # or /dev/nbd*
-  do
-    if sudo losetup $i $FILE
-    then
-      DISK=$i
-      break
-    fi
-  done
-  [ "$DISK" == "" ] && fail "no loop device available"
+# Partition device
+echo "Partitioning $DISK..."
 
-  echo "Connected $FILE to $DISK"
+# format as GPT
+# See also https://systemd.io/DISCOVERABLE_PARTITIONS/
+# https://wiki.archlinux.org/title/GPT_fdisk
+sudo sgdisk -Z $DISK
 
-  # Partition device
-  echo "Partitioning $DISK..."
-
-  # format as GPT
-  # See also https://systemd.io/DISCOVERABLE_PARTITIONS/
-  # https://wiki.archlinux.org/title/GPT_fdisk
-  sudo sgdisk -Z $DISK
-
-  # Add efi partition
-  if [ "$TARGET" != "rootfs" ]; then
-    sudo sgdisk -n 0:0:+510M  -t 0:ef00 -c 0:"efi_vm" $DISK
-    sudo mkfs.vfat -F 32 -n EFI -i 10000000 ${DISK}p1 || fail "cannot create efi"
-    sudo mount ${DISK}p1 $MNT_EFI || fail "cannot mount"
-  fi
-
-  sudo sgdisk -n 0:0:       -t 0:8304 -c 0:"linux_vm" $DISK
+# Add efi partition
+if [ "$TARGET" != "rootfs" ]; then
+  sudo sgdisk -n 0:0:+480M  -t 0:ef00 -c 0:"efi_vm" $DISK
   sudo partprobe $DISK
+  sudo mkfs.vfat -F 32 -n EFI -i 10000000 ${DISK}p1 || fail "cannot create efi"
+  sudo mount ${DISK}p1 $MNT_EFI || fail "cannot mount"
+fi
 
-  echo "Creating filesystems..."
-  sudo mkfs.btrfs -L "linux" -U 10000000-0000-0000-0000-000000000000 ${DISK}p2 || fail "cannot create root"
+sudo sgdisk -n 0:0:       -t 0:8304 -c 0:"linux_vm" $DISK
+sudo partprobe $DISK
 
-  # Todo - test compress
-  sudo mount -o compress ${DISK}p2 $MNT || fail "cannot mount"
-  sudo chmod g+w  $MNT
-  sudo btrfs subvolume create $MNT/linux
-  sudo chmod g+w  $MNT/linux
+echo "Creating filesystems..."
+sudo mkfs.btrfs -L "linux" -U 10000000-0000-0000-0000-000000000000 ${DISK}p2 || fail "cannot create root"
 
-  cd $MNT/linux
-  sudo btrfs subvolume set-default .
+# Todo - test compress
+sudo mount -o compress ${DISK}p2 $MNT || fail "cannot mount"
+sudo chmod g+w  $MNT
+sudo btrfs subvolume create $MNT/linux
+sudo chmod g+w  $MNT/linux
+
+cd $MNT/linux
+sudo btrfs subvolume set-default .
 
 if [ -z $2 ]; then
   infra-get-rootfs.sh $MNT/linux
@@ -83,10 +84,10 @@ else
   sudo rsync -av $2 $MNT/linux/
 fi
 
-  # Make it read-only
-  sudo btrfs property set -ts $MNT/linux ro true
-  cd /
-  sudo umount $MNT
+# Make it read-only
+sudo btrfs property set -ts $MNT/linux ro true
+cd /
+sudo umount $MNT
 
 if [ -z $1 ]; then
   infra-get-efi.sh
@@ -95,17 +96,16 @@ else
   sudo rsync -av $1 $MNT_EFI
 fi
 
-  # https://wiki.archlinux.org/title/Syslinux
-  sudo sgdisk $DISK --attributes=1:set:2
-  sudo dd bs=440 count=1 conv=notrunc if=$MNT_EFI/syslinux/gptmbr.bin of=$DISK
-  sudo extlinux --install $MNT_EFI/syslinux/
-  cd /
+# https://wiki.archlinux.org/title/Syslinux
+sudo sgdisk $DISK --attributes=1:set:2
+sudo dd bs=440 count=1 conv=notrunc if=$MNT_EFI/syslinux/gptmbr.bin of=$DISK
+sudo extlinux --install $MNT_EFI/syslinux/
+cd /
 
-  # directories that are not needed for vm
-  if [ "$TARGET" == vm ]; then
-    sudo rm -rf $MNT_EFI/syslinux $MNT_EFI/tce
-  fi
+# directories that are not needed for vm
+if [ "$TARGET" == vm ]; then
+  sudo rm -rf $MNT_EFI/syslinux $MNT_EFI/tce
+fi
 
-  sudo umount $MNT_EFI
-
-  sudo losetup -d /dev/loop* 2>/dev/null
+sudo umount $MNT_EFI
+sudo losetup -d /dev/loop* 2>/dev/null
