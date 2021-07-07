302f45449b9354353c95877c017db15a7c73cd55
Laszlo Gombos <l.gombos@samsung.com>
2021-07-07 08:20:37 -0400

    Disable chrome customization for now

--- bin/my-launcher.sh
+++ bin/my-launcher.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-active_window_name=$(xdotool getactivewindow getwindowname)
+#active_window_name=$(xdotool getactivewindow getwindowname)
 
-if echo $active_window_name | grep -i "Google Chrome"
-then
-  /home/user/.bin/rofi-pass
-else
+#if echo $active_window_name | grep -i "Google Chrome"
+#then
+#  /home/user/.bin/rofi-pass
+#else
   rofi -combi-modi window,drun -show combi -modi combi
-fi
+#fi
