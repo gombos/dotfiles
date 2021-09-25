e617ac130377573e5fdbf20eb5bfd5f316134b32
gombi <gombi@>
2021-09-24 21:05:09 -0400

    Document boot & rescue disk

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -1,5 +1,5 @@
-#wget https://download.manjaro.org/xfce/21.1.3/manjaro-xfce-21.1.3-210916-linux513.iso
-#wget https://dl.fedoraproject.org/pub/fedora/linux/releases/34/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-34-1.2.iso
-#wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso
+wget https://download.manjaro.org/xfce/21.1.3/manjaro-xfce-21.1.3-210916-linux513.iso
+wget https://dl.fedoraproject.org/pub/fedora/linux/releases/34/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-34-1.2.iso
+wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso
 wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.04/systemrescue-8.04-amd64.iso/download -O systemrescue-8.04-amd64.iso
-#wget https://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso
+wget https://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso
