5b9ef3f2e1161bccf8d91f216f7d5a0ff2b1c2da
gombi <gombi@>
2021-09-27 09:59:04 -0400

    Fix digikam

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -3,3 +3,7 @@ wget https://dl.fedoraproject.org/pub/fedora/linux/releases/34/Workstation/x86_6
 wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso
 wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.04/systemrescue-8.04-amd64.iso/download -O systemrescue-8.04-amd64.iso
 wget https://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso
+
+
+wget https://download.kde.org/unstable/digikam/digiKam-7.2.0-rc-x86-64.appimage
+#https://download.kde.org/stable/digikam/7.3.0/digiKam-7.3.0-x86-64.appimage
