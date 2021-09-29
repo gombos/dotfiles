49583c9246178b9c1413a9a4d93fa78ffc8227d9
gombi <gombi@>
2021-09-29 11:04:30 -0400

    Reduce complexity

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -1,10 +1,5 @@
-wget https://download.manjaro.org/xfce/21.1.3/manjaro-xfce-21.1.3-210916-linux513.iso
 wget https://dl.fedoraproject.org/pub/fedora/linux/releases/34/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-34-1.2.iso
 wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso
 wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.04/systemrescue-8.04-amd64.iso/download -O systemrescue-8.04-amd64.iso
-wget https://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso
-
-
-wget https://download.kde.org/unstable/digikam/digiKam-7.2.0-rc-x86-64.appimage
-#https://download.kde.org/stable/digikam/7.3.0/digiKam-7.3.0-x86-64.appimage
+wget https://download.kde.org/stable/digikam/7.3.0/digiKam-7.3.0-x86-64.appimage
 wget http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso
