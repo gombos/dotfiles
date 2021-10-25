b709e4b039fadbf6945e4d3926baa353e59ecc1c
gombi <gombi@>
2021-10-24 22:33:06 -0400

    Good enough

--- bin/infra-env.sh
+++ bin/infra-env.sh
@@ -4,5 +4,10 @@ export KERNEL="5.11.0-34-generic"
 export NVIDIA="460"
 
 # staging
+#export RELEASE="hirsute"
+#export NVIDIA="470"
+
 #export RELEASE="impish"
 #export KERNEL="5.13.0-19-generic"
+# todo - bgfiler - kernel bug
+# todo - systemd or some other rootfs update does not like the vmware service files, so vmware modules load but service does not start
