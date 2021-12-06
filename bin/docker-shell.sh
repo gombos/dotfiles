3397598a1888e7b1b3e3213ec3d84c0596911cc3
gombi <gombi@>
2021-12-06 09:42:11 -0500

    default arguments

--- bin/docker-shell.sh
+++ bin/docker-shell.sh
@@ -1,1 +1,13 @@
-sudo docker run --tty --interactive --rm --volume /home/:/home --workdir "$PWD" $1 /bin/sh
+i=$1
+
+if [ -z "$i" ]; then
+  i="0gombi0/homelab"
+fi
+
+b=$(docker run $i sh -c "test -f /bin/bash && echo bash")
+
+if [ "$b" = "bash" ]; then
+  sudo docker run --tty --interactive --rm --volume /home/:/home --workdir "$PWD" $i /bin/bash
+else
+  sudo docker run --tty --interactive --rm --volume /home/:/home --workdir "$PWD" $i /bin/sh
+fi
