21c27cd39ef41f5a523ed805fdef10cb0fa919ea
gombi <gombi@>
2021-12-05 20:31:56 -0500

    Add networking to qemu instance

--- bin/infra-qemu.sh
+++ bin/infra-qemu.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -nodefaults -snapshot -cpu host -boot d -cdrom  ~/linux.iso -m 4G -machine type=q35,accel=hvf -smp 2 -display default,show-cursor=on -device VGA
+qemu-system-x86_64 -nodefaults -snapshot -cpu host -boot d -cdrom  ~/linux.iso -m 4G -machine type=q35,accel=hvf -smp 2 -display default,show-cursor=on -device VGA -net user -net nic
