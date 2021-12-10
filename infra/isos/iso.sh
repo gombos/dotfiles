ca8fbae4393b46aea73dbab58c6726f1c96ae994
gombi <gombi@>
2021-12-10 17:14:32 -0500

    Make it easier to maintain

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -1,4 +1,4 @@
-wget https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso
-wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso
-wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.04/systemrescue-8.04-amd64.iso/download -O systemrescue-8.04-amd64.iso
-wget http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso
+wget https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso -O Fedora-Workstation.iso
+wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso -O lubuntu.iso
+wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.06/systemrescue-8.06-amd64.iso/download -O systemrescue.iso
+wget http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso -O KNOPPIX.iso
