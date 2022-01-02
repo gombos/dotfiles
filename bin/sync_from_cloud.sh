6c1071a182511cd523c767581e256dd532994e29
gombi <gombi@>
2022-01-02 16:00:39 -0500

    view docx from less

--- bin/sync_from_cloud.sh
+++ bin/sync_from_cloud.sh
@@ -4,6 +4,6 @@
 #git -C /run/media/backup_bestia/gitlab/dotfiles.clone pull
 #git -C /run/media/backup_bestia/gitlab/dotfiles.clone gc
 
-rclone sync --exclude Henrik_MeadowBrook/** --exclude Henrik/** gombidrive: ~/gombidrive
+rclone sync --exclude Henrik_MeadowBrook/** gombidrive: ~/gombidrive
 rclone sync --exclude Henrik_MeadowBrook/** henrikdrive: ~/henrikdrive
 rclone sync cadrive: ~/cadrive
