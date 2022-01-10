5c147ad0b54afd856217a568e8845ad256a61cdc
gombi <gombi@>
2022-01-10 14:49:18 -0500

    Export nfs on demand to full network

--- bin/nfs-export.sh
+++ bin/nfs-export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$1 192.168.1.0/28(rw,sync,wdelay,hide,nocrossmnt,insecure,root_squash,all_squash,no_subtree_check,secure_locks,acl,no_pnfs,anonuid=1000,anongid=1000,sec=sys)" | sudo tee -a /var/lib/nfs/etab
-echo "$1 192.168.1.0/28(rw,sync,wdelay,hide,nocrossmnt,insecure,root_squash,all_squash,no_subtree_check,secure_locks,acl,no_pnfs,anonuid=1000,anongid=1000,sec=sys)" | sudo tee -a /etc/exports
+echo "$1 192.168.1.0/24(rw,sync,wdelay,hide,nocrossmnt,insecure,root_squash,all_squash,no_subtree_check,secure_locks,acl,no_pnfs,anonuid=1000,anongid=1000,sec=sys)" | sudo tee -a /var/lib/nfs/etab
+echo "$1 192.168.1.0/24(rw,sync,wdelay,hide,nocrossmnt,insecure,root_squash,all_squash,no_subtree_check,secure_locks,acl,no_pnfs,anonuid=1000,anongid=1000,sec=sys)" | sudo tee -a /etc/exports
 
 sudo systemctl restart nfs-server
