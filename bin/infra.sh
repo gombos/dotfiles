45b1ac0116281606fdf3b7f29fd24ec7ea8fa2e1
gombi <gombi@>
2022-02-05 00:41:17 -0500

    Add non-root user

--- bin/infra.sh
+++ bin/infra.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#apt-get purge -y -q byobu pastebinit linux-headers-generic snapd
-
-#apt-mark hold linux-image-generic
-
 echo "RESUME=none" > /etc/initramfs-tools/conf.d/noresume.conf
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+adduser --disabled-password --gecos "" usr --ingroup adm && usermod -aG sudo,netdev admin
+
+#apt-get purge -y -q byobu pastebinit linux-headers-generic snapd secureboot-db libpackagekit* libplist* rsyslog
+
+#apt-mark hold linux-image-generic linux-image-amd64
 
 #apt-get purge -y -q cryptsetup-initramfs libplymouth* libntfs-*
 
-#apt-get update
+apt-get update
 
-#apt-get install -y -qq --no-install-recommends joe
+apt-get install -y -qq --no-install-recommends micro git
 
-#apt-get purge -y -q secureboot-db libpackagekit* libplist*
-
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+runuser -u usr -- git clone https://github.com/gombos/dotfiles.git .dotfiles
