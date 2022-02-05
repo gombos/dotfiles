ba84a142feadf05330a14e06f70da312db529b84
gombi <gombi@>
2022-02-05 11:27:24 -0500

    provision user

--- bin/infra.sh
+++ bin/infra.sh
@@ -19,18 +19,19 @@ head -1 /etc/shadow | sed -e 's/^root/usr/' >> /etc/shadow
 mv /root/.ssh/authorized_keys /home/usr/.ssh/
 chmod 400 /home/usr/.ssh/authorized_keys
 chown -R usr:usr /home/usr/.ssh/
+rm -rf /root/.ssh
+
+apt-mark hold linux-image-generic linux-image-amd64
 
 #apt-get purge -y -q byobu pastebinit linux-headers-generic snapd secureboot-db libpackagekit* libplist* rsyslog
-
-#apt-mark hold linux-image-generic linux-image-amd64
-
 #apt-get purge -y -q cryptsetup-initramfs libplymouth* libntfs-*
 
 apt-get update
 
-apt-get install -y -qq --no-install-recommends micro git
+apt-get install -y -qq --no-install-recommends git
 
 runuser -u usr -- git clone https://github.com/gombos/dotfiles.git /home/usr/.dotfiles
+runuser -u usr -- /home/usr/.dotfiles/bin/infra-provision-user.sh
 
-#Disable root login
+# Disable root login
 usermod -p '*' root
