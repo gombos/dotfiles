175a4950544d5d543f954038ed9e063746062b97
gombi <gombi@>
2022-02-05 00:48:18 -0500

    Fixs

--- bin/infra.sh
+++ bin/infra.sh
@@ -3,7 +3,7 @@
 echo "RESUME=none" > /etc/initramfs-tools/conf.d/noresume.conf
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 
-adduser --disabled-password --gecos "" usr --ingroup adm && usermod -aG sudo,netdev admin
+adduser --disabled-password --gecos "" usr && usermod -aG adm,sudo,netdev admin
 
 #apt-get purge -y -q byobu pastebinit linux-headers-generic snapd secureboot-db libpackagekit* libplist* rsyslog
 
@@ -15,4 +15,4 @@ apt-get update
 
 apt-get install -y -qq --no-install-recommends micro git
 
-runuser -u usr -- git clone https://github.com/gombos/dotfiles.git .dotfiles
+runuser -u usr -- git clone https://github.com/gombos/dotfiles.git ~/.dotfiles
