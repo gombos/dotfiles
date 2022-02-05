f72d0f49329e7d5da740989b180fa337165a3b65
gombi <gombi@>
2022-02-05 07:55:34 -0500

    Add key to the usr

--- bin/infra.sh
+++ bin/infra.sh
@@ -2,8 +2,13 @@
 
 echo "RESUME=none" > /etc/initramfs-tools/conf.d/noresume.conf
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 
 adduser --disabled-password --gecos "" usr && usermod -aG adm,sudo,netdev usr
+mkdir -p /home/usr/.ssh
+mv /root/.ssh/authorized_keys /home/usr/.ssh/
+chmod 400 /home/usr/.ssh/authorized_keys
+chown -R usr:usr /home/usr/
 
 #apt-get purge -y -q byobu pastebinit linux-headers-generic snapd secureboot-db libpackagekit* libplist* rsyslog
 
@@ -18,3 +23,5 @@ apt-get install -y -qq --no-install-recommends micro git
 rm -rf /home/usr/.*
 
 runuser -u usr -- git clone https://github.com/gombos/dotfiles.git /home/usr/.dotfiles
+
+#todo - disable root user as much as possible
