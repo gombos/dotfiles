77eaef917aec768563c8a8f4a4a2fc5a08dcf9f3
gombi <gombi@>
2022-02-12 19:47:55 -0500

    confused

--- bin/infra.sh
+++ bin/infra.sh
@@ -171,10 +171,4 @@ THEEND
 # cleanup
 infra-clean-linux.sh /
 rm -rf tmp/*
-
-#HOST=$(cat /etc/hostname)
-
-# Boot into ISO if executed on pincer
-#if [ "$HOST" == "pincer" ]; then
-  reboot
-#fi
+reboot
