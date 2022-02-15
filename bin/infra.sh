ed83d69fc6a985b6c0d8289b66b5eaac02c24776
gombi <gombi@>
2022-02-15 09:11:26 -0500

    reuse logic

--- bin/infra.sh
+++ bin/infra.sh
@@ -50,8 +50,9 @@ if [ -n "$USR" ]; then
   echo $SHADOW >> /etc/shadow
 
   #echo $SHADOW > /config/shadow
-  echo -n "IPWD='" >> /config/rootfs-kulcs.cfg
+  echo -n "GOMBIPWD='" >> /config/rootfs-kulcs.cfg
   echo $SHADOW | cut -d: -f2 | tr '\n' "'" >> /config/rootfs-kulcs.cfg
+  echo "" >> /config/rootfs-kulcs.cfg
 
   # Todo - elevate this to the iso as well
 
