30729b296c9c9989595d1520c701df91a5334206
gombi <gombi@>
2022-02-15 08:39:24 -0500

    fix newline

--- bin/infra.sh
+++ bin/infra.sh
@@ -51,7 +51,7 @@ if [ -n "$USR" ]; then
 
   #echo $SHADOW > /config/shadow
   echo -n "IPWD=\'" >> /config/rootfs-kulcs.cfg
-  echo -n $SHADOW | cut -d: -f2 >> /config/rootfs-kulcs.cfg
+  echo -n $SHADOW | cut -d: -f2 | tr '\n' "'" >> /config/rootfs-kulcs.cfg
   echo "\'" >> /config/rootfs-kulcs.cfg
 
   # Todo - elevate this to the iso as well
