d556b6aa380b9f16bbaebff3778fbe7018f97798
gombi <gombi@>
2022-02-15 17:35:05 -0500

    organize better

--- bin/infra.sh
+++ bin/infra.sh
@@ -5,23 +5,21 @@
 # It should not run at each boot
 
 cd /
+mkdir -p /config/updates/etc/ssh /config/updates/etc/network
 
 if [ -n "$SCRIPT" ]; then
   eval $SCRIPT
-  mkdir -p /config/updates/etc/ssh /config/updates/etc/network
   echo $SCRIPT >> /config/rootfs-kulcs.cfg
-
-  # Elevete some files so that they are picked up by ISO
-  cp /etc/ssh/sshd_config  /config/updates/etc/ssh
-  cp /etc/network/interfaces /config/updates/etc/network
-
-  cp /etc/hostname /config/updates/etc
-  cp /etc/hosts /config/updates/etc
-  cp /etc/rsyslog.conf /config/updates/etc
 fi
 
-# Might run at first boot, services might be already running
+# Elevete some files so that they are picked up by ISO
+cp /etc/ssh/sshd_config  /config/updates/etc/ssh
+cp /etc/network/interfaces /config/updates/etc/network
 
+cp /etc/hosts /config/updates/etc
+cp /etc/rsyslog.conf /config/updates/etc
+
+# Might run at first boot, services might be already running
 echo "PasswordAuthentication no" >> /config/updates/etc/ssh/sshd_config
 echo "ChallengeResponseAuthentication no" >> /config/updates/etc/ssh/sshd_config
 echo "UsePAM no" >> /config/updates/etc/ssh/sshd_config
@@ -35,7 +33,18 @@ if [ -n "$SSHDPORT" ]; then
   echo "Port ${SSHDPORT}" >> /config/updates/etc/ssh/sshd_config
 fi
 
-# Things only run in the cloud
+[ -n "$LABEL" ] && echo "$LABEL" > /config/updates/etc/hostname
+[ -n "$LABEL" ] && echo "127.0.0.1 $LABEL" >> /config/updates/etc/hosts
+
+if [ -n "$LOG" ]; then
+  echo "*.*                     @${LOG}" >> /config/updates/etc/rsyslog.conf
+fi
+
+cat > /config/grub.cfg << 'EOF'
+isolabel=linode-root
+OVERRIDE="systemd.unit=multi-user.target systemd.want=getty@tty1.service console=ttyS0,19200n8 systemd.hostname=pincer systemd.mask=home systemd.mask=NetworkManager systemd.mask=NetworkManager-wait-online"
+EOF
+
 if [ -n "$USR" ]; then
   # Take password from root
   echo -n "GOMBIPWD='" >> /config/rootfs-kulcs.cfg
@@ -57,10 +66,6 @@ if [ -n "$USR" ]; then
   git clone https://github.com/gombos/dotfiles.git /home/$USR/.dotfiles
   chown -R 1000:1000 /home/$USR
 
-  if [ -d /home/$USR/.dotfiles/bin ]; then
-    PATH=/home/$USR/.dotfiles/bin:$PATH
-  fi
-
   mkdir -p isos/
   cd isos
   wget --quiet https://github.com/gombos/dotfiles/releases/download/iso/linux.iso
@@ -68,24 +73,8 @@ if [ -n "$USR" ]; then
 
   cp /home/$USR/.dotfiles/boot/grub.cfg /boot/grub/custom.cfg
 
-cat > /config/grub.cfg << 'EOF'
-isolabel=linode-root
-OVERRIDE="systemd.unit=multi-user.target systemd.want=getty@tty1.service console=ttyS0,19200n8 systemd.hostname=pincer systemd.mask=home systemd.mask=NetworkManager systemd.mask=NetworkManager-wait-online"
-EOF
-
-fi
-
-[ -n "$LABEL" ] && echo "$LABEL" > /config/updates/etc/hostname
-[ -n "$LABEL" ] && echo "127.0.0.1 $LABEL" >> /config/updates/etc/hosts
-
-if [ -n "$LOG" ]; then
-echo "*.*                   	@${LOG}" >> /config/updates/etc/rsyslog.conf
-fi
-
-# Only on Linode
-if [ -n "$SCRIPT" ]; then
   # Run as root when iso boots - runs at each boot
   ln -sf /run/initramfs/isoscan/home/$USR/.dotfiles/bin/infra-boots.sh /config/infra-boots.sh
-
-  reboot
 fi
+
+/sbin/reboot
