5286d71ae992dc08ca38430fb4c5df4a36b18e27
gombi <gombi@>
2022-02-17 07:36:25 -0500

    fresh mind

--- bin/dmsquash-live-root.sh
+++ bin/dmsquash-live-root.sh
@@ -129,7 +129,16 @@ do_live_overlay() {
     # need to know where to look for the overlay
     if [ -z "$setup" -a -n "$devspec" -a -n "$pathspec" -a -n "$overlay" ]; then
         mkdir -m 0755 -p /run/initramfs/overlayfs
-        mount -n -t auto "$devspec" /run/initramfs/overlayfs || :
+
+        if ismounted "$devspec"; then
+          echo 4
+          devmp=find_mount($pathspec)
+          echo $devmp
+          mount --bind "$devmp" /run/initramfs/overlayfs
+        else
+          mount -n -t auto "$devspec" /run/initramfs/overlayfs || :
+        fi
+
         if [ -f /run/initramfs/overlayfs$pathspec -a -w /run/initramfs/overlayfs$pathspec ]; then
             OVERLAY_LOOPDEV=$(losetup -f --show ${readonly_overlay:+-r} /run/initramfs/overlayfs$pathspec)
             over=$OVERLAY_LOOPDEV
