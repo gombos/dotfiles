6ca2d511adbe162aabe33f2292f4fa75f14c3b2b
gombi <gombi@>
2022-02-18 15:08:16 -0500

    eliminate

--- bin/infra-build-boot.sh
+++ bin/infra-build-boot.sh
@@ -153,22 +153,24 @@ rm -rf usr/sbin/dmsetup
 rm -rf usr/lib/modules/$KERNEL/kernel/drivers/md
 
 # optimize - this does not remove the dependent libraries
-#rm -rf usr/sbin/chroot
-#rm -rf usr/sbin/rmmod
+rm -rf usr/sbin/chroot
+rm -rf usr/sbin/rmmod
 #rm -rf usr/bin/uname
-#rm -rf usr/bin/dmesg
-#rm -rf usr/bin/kmod
+rm -rf usr/bin/dmesg
+rm -rf usr/bin/kmod
 #rm -rf usr/bin/tar
 #rm -rf usr/bin/cpio
 #rm -rf usr/bin/bzip2
 #rm -rf usr/bin/gzip
-#rm -rf usr/bin/chmod
+rm -rf usr/bin/chmod
+
 #rm -rf etc/cmdline.d
 #rm -rf etc/fstab.empty
 #rm -rf etc/conf.d
 #rm -rf etc/ld.so.conf
 #rm -rf etc/ld.so.conf.d/libc.conf
 #rm -rf var/tmp
+
 #rm -rf root
 
 # kexec can only handle one initrd file
