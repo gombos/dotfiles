08958ab2a850aae081d39dc30a694d3f8fd37c68
gombi <gombi@>
2022-03-17 12:57:11 +0000

    Update system rescue

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -1,5 +1,5 @@
 wget https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso -O Fedora-Workstation.iso
 wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso -O lubuntu.iso
-wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.06/systemrescue-8.06-amd64.iso/download -O systemrescue.iso
+wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/9.01/systemrescue-9.01-amd64.iso/download -O systemrescue.iso
 wget http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso -O KNOPPIX.iso
 wget https://github.com/gombos/dotfiles/releases/download/iso/linux.iso
