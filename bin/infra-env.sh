b10770f26e3fc39fe7797654b81fbbb3a9864c7b
gombi <gombi@>
2022-04-01 14:41:23 +0000

    switch kernel

--- bin/infra-env.sh
+++ bin/infra-env.sh
@@ -1,7 +1,7 @@
 export RELEASE="jammy"
-#export KERNEL="5.15.0-22-generic"
+export KERNEL="5.15.0-23-generic"
 
 # old
 #export NVIDIA="470"
 #export RELEASE="impish"
-export KERNEL="5.13.0-35-generic"
+#export KERNEL="5.13.0-35-generic"
