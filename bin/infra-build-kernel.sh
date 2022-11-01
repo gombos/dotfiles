a3681c1f7bd4a71ece3f0e651adadd24ac54fd89
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-11-01 19:39:57 +0000

    Updated kernel does not boot on nagybestia

--- bin/infra-build-kernel.sh
+++ bin/infra-build-kernel.sh
@@ -63,11 +63,13 @@ apt-get install -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 cpio
 mkdir /tmp/dracut
 
 dracut --quiet --nofscks --force --no-hostonly --no-early-microcode --no-compress --tmpdir /tmp/dracut --keep --kernel-only \
-  --add-drivers 'autofs4 overlay nls_iso8859_1 isofs ntfs ahci nvme xhci_pci uas sdhci_acpi mmc_block pata_acpi virtio_scsi' \
+  --add-drivers 'autofs4 overlay nls_iso8859_1 isofs ntfs ahci nvme xhci_pci uas sdhci_acpi mmc_block pata_acpi virtio_scsi usbhid hid_generic hid' \
   --modules 'rootfs-block' \
   initrd.img $KERNEL
 
 cd  /tmp/dracut/dracut.*/initramfs/
+
+find lib/modules/ -name "*.ko"
 
 find lib/modules/ -print0 | cpio --null --create --format=newc | gzip --best > /efi/kernel/initrd_modules.img
 
