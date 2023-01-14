c41ebda26a4f4857688e428aa4f2cb46c0b5f197
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-14 22:49:27 +0000

    test

--- bin/infra-usrlocal.sh
+++ bin/infra-usrlocal.sh
@@ -7,6 +7,6 @@ if ! [ -z "$REPO" ]; then
 fi
 
 TMPUSRLOCAL=/tmp/usrlocal
-sudo rm -rf $TMPUSRLOCAL && sudo mkdir $TMPUSRLOCAL && cd $TMPUSRLOCAL
-docker run --privileged --device /dev/fuse --cap-add SYS_ADMIN -v $TMPUSRLOCAL:/usr/local -v ~/.dotfiles/bin/:/tmp/bin 0gombi0/homelab-baremetal:extra /tmp/bin/packages-usrlocal
-sudo mksquashfs $TMPUSRLOCAL /tmp/usrlocal.img -comp zstd
+rm -rf $TMPUSRLOCAL && mkdir $TMPUSRLOCAL && cd $TMPUSRLOCAL
+/tmp/packages-usrlocal
+mksquashfs $TMPUSRLOCAL /tmp/usrlocal.img -comp zstd
