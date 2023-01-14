ac3050c631f46cc2ec09c9db65e658ff490c193c
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-14 23:04:16 +0000

    test

--- bin/infra-usrlocal.sh
+++ bin/infra-usrlocal.sh
@@ -6,7 +6,7 @@ cd /tmp
 
 TMPUSRLOCAL=/tmp/usrlocal
 rm -rf $TMPUSRLOCAL && mkdir $TMPUSRLOCAL && cd $TMPUSRLOCAL
-./packages-usrlocal
+. ./packages-usrlocal
 mksquashfs $TMPUSRLOCAL /tmp/usrlocal.img -comp zstd
 find /tmp
 find $TMPUSRLOCAL
