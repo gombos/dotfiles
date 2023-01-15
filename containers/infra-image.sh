4f0c381f7448755dd04a6fe7d1cf0d5092eb6a3d
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-15 00:40:58 +0000

    test

--- containers/infra-image.sh
+++ containers/infra-image.sh
@@ -18,9 +18,7 @@ else
   TARGET="$1"
 fi
 
-#FILENAME=linux.img
 OUT_DIR=${OUT_DIR:=/tmp}
-#FILE=$OUT_DIR/${FILENAME}
 MNT_DIR=${MNT_DIR:=$OUT_DIR/mnt}
 MNT=$MNT_DIR/root
 MNT_EFI=$MNT_DIR/efi
@@ -28,47 +26,6 @@ MNT_EFI=$MNT_DIR/efi
 sudo rm -rf $MNT $MNT_EFI $MNT_DIR
 
 sudo mkdir -p $MNT $MNT_EFI
-#echo "Installing $RELEASE into $FILE..."
-
-# 3GB image file to fit comfortable to 8 GB sticks or larger
-#IMGSIZE=${IMGSIZE:=3072} # in megabytes
-#EFISIZE=${EFISIZE:=300} # in megabytes
-#MODSIZE=${MODSIZE:=180} # in megabytes
-
-#if [ -f $FILE ]; then
-#  sudo rm -rf $FILE
-#fi
-
-#if [ ! -f $FILE ]; then
-#  echo "Creating $FILE"
-#  sudo dd if=/dev/zero of=$FILE bs=1024k seek=${IMGSIZE} count=0
-#fi
-
-# Find an empty loopback device
-#DISK=""
-#for i in /dev/loop* # or /dev/nbd*
-#do
-#  if sudo losetup $i $FILE
-#  then
-#    DISK=$i
-#    break
-#  fi
-#done
-#[ "$DISK" == "" ] && fail "no loop device available"
-
-# Partition device
-#echo "Partitioning $DISK..."
-
-# format as GPT
-# See also https://systemd.io/DISCOVERABLE_PARTITIONS/
-# https://wiki.archlinux.org/title/GPT_fdisk
-#sudo sgdisk -Z $DISK
-
-# Add efi partition
-#sudo sgdisk -n 0:0:+${EFISIZE}M  -t 0:ef00 -c 0:"efi_linux" $DISK
-#sudo partprobe $DISK
-#sudo mkfs.vfat -F 32 -n EFI -i 10000000 ${DISK}p1 || fail "cannot create efi"
-#sudo mount ${DISK}p1 $MNT_EFI || fail "cannot mount"
 
 sudo mkdir /tmp/iso/
 
@@ -81,53 +38,6 @@ fi
 
 ls -la /tmp/efi/kernel
 ls -la /tmp/iso/kernel
-
-# https://wiki.archlinux.org/title/Syslinux
-#sudo sgdisk $DISK --attributes=1:set:2
-#sudo dd bs=440 count=1 conv=notrunc if=$MNT_EFI/syslinux/gptmbr.bin of=$DISK
-#sudo extlinux --install $MNT_EFI/syslinux/
-#cd /
-
-# directories that are not needed for vm
-#if [ "$TARGET" == vm ]; then
-#  sudo rm -rf $MNT_EFI/syslinux $MNT_EFI/tce
-#fi
-
-#sudo rsync -av $MNT_EFI/ /tmp/iso/
-
-#sudo umount $MNT_EFI
-#sudo losetup -d $DISK
-
-#sudo losetup $DISK $FILE
-#sudo partprobe $DISK
-
-#sudo mount ${DISK}p1 $MNT_EFI || fail "cannot mount"
-#sudo umount $MNT_EFI
-
-#sudo sgdisk -n 0:0: -t 0:8304 -c 0:"linux_linux" $DISK
-
-#sudo partprobe $DISK
-
-#echo "Creating filesystems..."
-#sudo mkfs.btrfs -L "linux" -U 10000000-0000-0000-0000-000000000000 ${DISK}p2 || fail "cannot create root"
-
-#sudo mount -o compress=zstd ${DISK}p2 $MNT || fail "cannot mount"
-#sudo chmod g+w  $MNT
-#sudo btrfs subvolume create $MNT/linux
-#sudo chmod g+w  $MNT/linux
-
-#cd $MNT/linux
-#sudo btrfs subvolume set-default .
-
-#if [ -z $3 ]; then
-#  infra-get-rootfs.sh $MNT/linux
-#else
-#  sudo rsync -av $3 $MNT/linux/
-#fi
-
-# Make it read-only
-#sudo btrfs property set -ts $MNT/linux ro true
-#cd /
 
 # rootfs squashfs
 #infra-get-squash.sh
@@ -183,48 +93,6 @@ sudo chown -R 1000:1000 .
 sudo mkdir /tmp/isotemp
 sudo mv isolinux/bios.img /tmp/isotemp/
 sudo mv isolinux/efiboot.img /tmp/isotemp/
-
-# todo combine boot and syslinux directories into biosboot directory
-# todo rename isolinux to isoboot
-# todo rename LiveOS to rootfs
-# todo rename BOOT to boot
-# todo - is this really needed - /EFI/efiboot.img=../isotemp/efiboot.img , maybe needed for dd
-
-# 2nd partition -  0xef - ef EFI (FAT-12/16/
-# /tmp/home.img \
-
-# hybrid - ISO can be written bit-for-bit to a USB device to make it a working Live USB
-# the resulting image will be MBR partitioned (and not GPT), which can boot both in
-# BIOS (BIOS-disk and BIOS-cdrom) and EFI mode (bootx64.efi)
-
-# To mount on MacOS
- # hdiutil attach -nobrowse -nomount linux.iso
- # mount -t cd9660 /dev/disk4 /tmp/cd
- # umount /tmp/cd
- # hdiutil detach /dev/disk4
-
-# Using xorriso on MacOS
-  #xorriso -indev linux.iso -osirrox on -extract / linux
-
-# ISO use cases:
-# copy iso file into e.g. /isos and chainboot into the iso (e.g. from grub)
-# use it cdrom file in a vm
-# extract/mount it and use parts - e.g. rootfs or qemu
-# mount it an copy files to a fat partition - cp -a /isomntpoint/* /usbmountpoint/ - will boot on uefi platform
-# extract, remaster and repackage
-# dd it into a drive (hybrid) and boot (BIOS of EFI) - Bitcopy (a.k.a "dd" )isoImage to usbstick
-
-# boot_hybrid.img - MBR
-# efiboot.img - MBR - vfat with EFI/BOOT/bootx64.efi in it
-
-# todo - remove graft points, makes it hard to reason about the steps
-# todo - -append_partition 2 0xef isolinux/efiboot.img \ - something like this is useful ?
-
-# todo - to hack kernel
-#cp /tmp/kernel/tmp/linux-5.15.32/arch/x86/boot/bzImage  /tmp/iso/kernel/vmlinuz
-
-# now this is linked into the kernel
-# rm -rf /tmp/iso/kernel/initrd.img
 
 find /tmp/iso
 
