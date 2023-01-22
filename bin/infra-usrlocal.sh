2f58c36cd3a9f64524a5bd8a25004dbe533f5db3
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-01-22 16:10:19 +0000

    test

--- bin/infra-usrlocal.sh
+++ bin/infra-usrlocal.sh
@@ -4,13 +4,9 @@ cp $REPO/bin/* /tmp/
 cp $REPO/packages/* /tmp/
 cd /tmp
 
-find .
-
 TMPUSRLOCAL=/tmp/usrlocal
 rm -rf $TMPUSRLOCAL && mkdir $TMPUSRLOCAL && cd $TMPUSRLOCAL
 . /tmp/packages-usrlocal
-mksquashfs $TMPUSRLOCAL /tmp/usrlocal.img -comp zstd
-find /tmp
-find $TMPUSRLOCAL
+mksquashfs /usr/local /tmp/usrlocal.img -comp zstd
 
 ls -la /tmp/usrlocal.img
