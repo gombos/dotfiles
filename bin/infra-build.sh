be674638a11ed2aaa320caa77a08cad0d0efcfe7
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-03-19 18:37:20 +0000

    move to github

--- bin/infra-build.sh
+++ bin/infra-build.sh
@@ -16,33 +16,3 @@ if echo $TARGET | grep -w -q kernel; then
   docker build -t ghcr.io/gombos/kernel     ~/.dotfiles/ -f ~/.dotfiles/containers/Dockerfile-kernel
   docker push     ghcr.io/gombos/kernel
 fi
-
-if echo $TARGET | grep -w -q kernelinitramfs; then
-  docker build -t ghcr.io/gombos/kernelinitramfs     ~/.dotfiles/ -f ~/.dotfiles/containers/Dockerfile-initrd2
-  docker push ghcr.io/gombos/kernelinitramfs
-fi
-
-if echo $TARGET | grep -w -q extra; then
-  docker build -t ghcr.io/gombos/rootfs-extra     ~/.dotfiles/ -f ~/.dotfiles/containers/Dockerfile-homelab-extra
-  docker push     ghcr.io/gombos/rootfs-extra
-fi
-
-if echo $TARGET | grep -w -q config; then
-  docker build -t ghcr.io/gombos/rootfs            ~/.dotfiles/ -f ~/.dotfiles/containers/Dockerfile-homelab-config
-  docker push     ghcr.io/gombos/rootfs
-fi
-
-if echo $TARGET | grep -w -q boot; then
-  docker build --tag ghcr.io/gombos/boot --file ~/.dotfiles/containers/Dockerfile-boot             ~/.dotfiles/
-  docker push     ghcr.io/gombos/boot
-fi
-
-if echo $TARGET | grep -w -q usrlocal; then
-  docker build --tag ghcr.io/gombos/usrlocal --file ~/.dotfiles/containers/Dockerfile-usrlocal             ~/.dotfiles/
-  docker push     ghcr.io/gombos/usrlocal
-fi
-
-if echo $TARGET | grep -w -q iso; then
-  docker build --tag ghcr.io/gombos/iso --file ~/.dotfiles/containers/Dockerfile-iso             ~/.dotfiles/
-  docker push     ghcr.io/gombos/iso
-fi
