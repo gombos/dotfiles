d8b1bcaa8d9191b4939250cd7104347b19bdb74e
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-03-20 02:42:36 +0000

    simplify

--- containers/kernel.sh
+++ containers/kernel.sh
@@ -1,4 +1,4 @@
-# builds for abpout 2:30 hours on GA
+# builds for about 2:30 hours on GA
 
 . $REPO/bin/infra-env.sh
 
@@ -78,55 +78,13 @@ rm -rf /boot/* /lib/modules/*
 make install
 make INSTALL_MOD_STRIP=1 modules_install
 
-#make headers_install
+# Make sure we have all the required modules built
+$REPO/bin/infra-install-vmware-workstation-modules.sh
 
-#apt-get purge -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 autoconf build-essential libssl-dev gawk openssl libssl-dev libelf-dev libudev-dev libpci-dev flex bison cpio zstd wget bc kmod git squashfs-tools cpio dracut-core ca-certificates apt-utils ca-certificates git fakeroot gzip dracut-core wget linux-base sudo libelf1 python3 dkms build-essential rsync
+#make headers_install
 
 find /boot/ /lib/modules/
 
 #/usr/include/
-
-exit
-
-mkdir -p /efi/kernel
-cp -r arch/x86/boot/bzImage /efi/kernel/vmlinuz
-
-file /efi/kernel/vmlinuz
-
-make -j16 modules
-make INSTALL_MOD_STRIP=1 modules_install
-make headers_install
-
-make clean
-
-mkdir /tmp/dracut
-
-KVERSION=$(cd /lib/modules; ls -1 | tail -1)
-
-find /usr/lib | grep .ko
-
-dracut --no-hostonly --kernel-only --no-compress --keep --tmpdir /tmp/dracut \
-  --add-drivers 'ntfs3 xhci_pci uas sdhci_acpi mmc_block pata_acpi virtio_scsi usbhid hid_generic hid' \
-  --modules 'rootfs-block' \
-  initrd.img $KVERSION
-
-cd  /tmp/dracut/dracut.*/initramfs/
-
-find lib/modules/ -name "*.ko"
-
-find lib/modules/ -print0 | cpio --null --create --format=newc | gzip --best > /efi/kernel/initrd_modules.img
-
-# Make sure we have all the required modules built
-$SCRIPTS/infra-install-vmware-workstation-modules.sh
-
-cd /tmp
-
-ls -lha /efi/kernel/initrd_modules.img
-
-mksquashfs /usr/lib/modules /efi/kernel/modules
-rm -rf /tmp/initrd
-
-find /efi/kernel
-ls -lRa /efi/kernel
-
-
+#make headers_install
+#make clean
