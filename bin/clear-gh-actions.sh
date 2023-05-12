42fdbd1df120f25d0765080e781f490145735147
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-05-12 03:32:31 +0000

    non-upstream prs

--- bin/clear-gh-actions.sh
+++ bin/clear-gh-actions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#user=LaszloGombos repo=dracut;
-user=initramfs-modules repo=initramfs-modules;
+user=LaszloGombos repo=dracut;
+#user=initramfs-modules repo=initramfs-modules;
 gh api repos/$user/$repo/actions/runs --paginate -q '.workflow_runs[] | "\(.id)"' | xargs -n1 -I % gh api repos/$user/$repo/actions/runs/% -X DELETE
