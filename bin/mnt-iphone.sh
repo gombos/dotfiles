fce015134af64d6b09b7be33c96be83519b048f8
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-05-28 23:27:28 +0000

    iphone mount

--- bin/mnt-iphone.sh
+++ bin/mnt-iphone.sh
@@ -1,5 +1,6 @@
 mkdir /mnt/iphone
 apt update
-apt install usbmuxd ifuse 
+apt install usbmuxd ifuse
+#apt install ideviceinstaller python3-imobiledevice libimobiledevice-utils python3-plist libusbmuxd-tools
 sudo usbmuxd
 sudo ifuse /mnt/iphone
