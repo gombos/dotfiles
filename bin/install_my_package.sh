50bc4f4d10d526168bdf581f9e32bd40273f97ba
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-07-02 17:06:33 -0400

    progress

--- bin/install_my_package.sh
+++ bin/install_my_package.sh
@@ -1,11 +1,11 @@
   if [ -f /usr/bin/apt-get ]; then
     apt-get install -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 "$@"
   else
-    if [ -f usr/local/bin/pacapt ]; then
+    if [ -f /usr/local/bin/pacapt ]; then
       if [ "$ID" = "alpine" ]; then
-        usr/local/bin/pacapt -S "$@"
+        /usr/local/bin/pacapt -Sy "$@"
       else
-        usr/local/bin/pacapt -S --noconfirm "$@"
+        /usr/local/bin/pacapt -Sy --noconfirm "$@"
       fi
     fi
   fi
