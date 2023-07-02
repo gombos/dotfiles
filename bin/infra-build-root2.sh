fb76cd029cbf1377daba62cbbf8f778ef7c6cb37
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-07-02 15:53:21 -0400

    start working on a container target

---
+++ bin/infra-build-root2.sh
@@ -0,0 +1,11 @@
+PATH=$PATH:.
+
+packages_update_db.sh
+
+install_my_packages.sh packages-core.l
+install_my_packages.sh packages-services.l
+install_my_packages.sh packages-x11.l
+install_my_packages.sh packages-x11apps.l
+install_my_packages.sh packages-filesystems.l
+install_my_packages.sh packages-packages.l
+install_my_packages.sh packages-extra.l
