afcf7c38f4a64a6bd5cdb2bad37c8802782853a1
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-07-03 15:38:18 -0400

    baseline

--- bin/infra-build-root2.sh
+++ bin/infra-build-root2.sh
@@ -24,14 +24,12 @@ PATH=$PATH:.
 #fi
 
 packages_update_db.sh
-
-install_my_packages.sh packages-core.l
-install_my_packages.sh packages-services.l
-install_my_packages.sh packages-x11core.l
-install_my_packages.sh packages-x11apps.l
-install_my_packages.sh packages-filesystems.l
-
 install_my_packages.sh packages-packages.l
-install_my_packages.sh packages-extra.l
-
 install_my_packages.sh  "packages*-$ID.l"
+
+#install_my_packages.sh packages-core.l
+#install_my_packages.sh packages-services.l
+#install_my_packages.sh packages-x11core.l
+#install_my_packages.sh packages-x11apps.l
+#install_my_packages.sh packages-filesystems.l
+#install_my_packages.sh packages-extra.l
