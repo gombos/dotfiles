b03ae0ea48785e04dbb9acb952b911a52c80178f
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-07-03 08:57:18 -0400

    prepare for arch

--- bin/install_my_package.sh
+++ bin/install_my_package.sh
@@ -1,11 +1,17 @@
-  if [ -f /usr/bin/apt-get ]; then
-    apt-get install -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 "$@"
+if [ -f /usr/bin/apt-get ]; then
+  apt-get install -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 "$@"
+  return
+fi
+
+if [ -f /usr/bin/paru ]; then
+  /usr/bin/paru /usr/local/bin/pacapt -Sy --noconfirm "$@"
+  return
+fi
+
+if [ -f /usr/local/bin/pacapt ]; then
+  if [ "$ID" = "alpine" ]; then
+    /usr/local/bin/pacapt -Sy "$@"
   else
-    if [ -f /usr/local/bin/pacapt ]; then
-      if [ "$ID" = "alpine" ]; then
-        /usr/local/bin/pacapt -Sy "$@"
-      else
-        /usr/local/bin/pacapt -Sy --noconfirm "$@"
-      fi
-    fi
+    /usr/local/bin/pacapt -Sy --noconfirm "$@"
   fi
+fi
