6362a64bf1d090f5e3fc1c98a58ae356b8401898
gombi <gombi@>
2023-07-23 10:22:16 -0400

    fix distrobox

--- bin/infra-reset.sh
+++ bin/infra-reset.sh
@@ -1,3 +1,3 @@
 docker pull ghcr.io/gombos/linux
-distrobox rm --force linux
-distrobox create --name linux --image ghcr.io/gombos/linux --volume /home:/home -Y --pre-init-hooks "sudo hostname $(uname -n)"
+distrobox rm --force my-distrobox
+distrobox create --name my-distrobox --image ghcr.io/gombos/linux --volume /run:/run  --volume /home:/home -Y --pre-init-hooks "sudo hostname $(uname -n)"
