a4d3d4db7ec8305b84a5ea06d4ccd91e961dfc1f
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-08-28 18:04:05 -0400

    better name

--- bin/infra-reset.sh
+++ bin/infra-reset.sh
@@ -1,4 +1,3 @@
 docker pull ghcr.io/gombos/linux:latest
-distrobox rm --force my-distrobox
 distrobox rm --force linux
-distrobox create --name linux --image ghcr.io/gombos/linux:linux --volume /run:/run  --volume /home:/home -Y --pre-init-hooks "sudo hostname $(uname -n)"
+distrobox create --name linux --image ghcr.io/gombos/linux:latest --volume /run:/run  --volume /home:/home -Y --pre-init-hooks "sudo hostname $(uname -n)"
