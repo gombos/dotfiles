13bc338414888d7ba39d8e774994b69249907fad
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-09-05 07:31:06 -0400

    try to get to 1 M

--- bin/infra-top-sizepackages.sh
+++ bin/infra-top-sizepackages.sh
@@ -1,1 +1,1 @@
-dpkg-query --show --showformat='${Installed-Size}\t${Package}\n' | sort -rh | head -40 | awk '{print $1/1024, $2}'
+dpkg-query --show --showformat='${Installed-Size}\t${Package}\n' | sort -rh | head -80 | awk '{print $1/1024, $2}'
