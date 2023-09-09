19824eb4f1f47008703f61534b5a802f9e9ea530
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-09-09 07:50:51 -0400

    update kernel

--- bin/infra-env.sh
+++ bin/infra-env.sh
@@ -1,7 +1,3 @@
-export KERNEL='6.1.36'
-#export RELEASE="lunar"
+export KERNEL='6.1.52'
 export RELEASE="bookworm"
-export RIPGREP="0.9.6"
-export BW="2022.6.2"
 export DIGIKAM=8.0.0
-#export RCLONE="1.58.0"
