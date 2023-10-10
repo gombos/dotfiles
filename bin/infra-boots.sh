835d5739c003476bcc6f3ce965d87109907be81f
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-10-09 21:17:25 -0400

    simplify

--- bin/infra-boots.sh
+++ bin/infra-boots.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
 
-# first boot of a new instance, run all the “per-instance” configuration
-
-# Warning: error in this file will likely lead to failure to boot
-
-# Idea: detect if cloud-init is installed in the image and run the script during later in the boot phase
-# Any of thid is needed when a container is initialized ?
-# For LXC-like containers, ssh keys are needed
-# also - thsi is a useful way to run containers - https://medium.com/swlh/docker-and-systemd-381dfd7e4628
-
-# Consider only testing/changing this file in a development environment with access to console
-
-# This script logs to /run/initramfs/rdexec.out
-# Determine where this script is called from - either as a dracut hook or as a cloud-init phase or 'docker run'
-
-# This script is optional for containers, that are not "booted" and do not initialize systemd. Keep it that way
-
-# cloud-init does similar things
-
-# Do not change /usr to allow ro mount
-# Ideally only make changes in /var
-# To avoid manipulating /etc use kernel arguments when possible
-# Instead of changing existing files, try to just add new files
-# Keep changing in /run
-
-# Configuration 1 - OverlayRoot
-# Rootfs is mounted ro and EFI partition is mounted as ro to customize rootfs overlay
-# /run can be used to create new files and directories that are not persistent between boots
-# Symlink /etc to /run when possible - use relative links. This will allow editing these files even if etc itself is mounted read-only
-
-# Input:
-# - environment variables, /proc filesystem, including /proc/cmdline
-# - current directory is set to where the rd.exec file is located
-# - $mp points to EFI partition root
-# - $NEWROOT points to root filesystem
-# - all changes to root filesystem are made in ram (overlayroot)
-
-# Autodetect the environment
-# Disks - /dev/disks, /proc/partitions
-# HW - /proc/cpuinfo
-# Grub - /proc/cmdline
-# Network - DHCP
-# rootfs version
-
-
-# TODO
-# move enabling/disabling systemd services out of this file into a kernel command line
-
 if [ -z "$NEWROOT" ]; then
   NEWROOT="/"
 fi
@@ -259,10 +212,6 @@ if [ "$HOST" = "bestia" ]; then
   #sed -i 's|\#port.*993|port=993\n    ssl=yes|g' $R/etc/dovecot/conf.d/10-master.conf
   #sed -i 's|mail_location.*|mail_location = maildir:~/Maildir:LAYOUT=fs|g' $R/etc/dovecot/conf.d/10-mail.conf
 
-  #nx
-  # todo - do not hardcode id in several places
-  chown -R 401:401 $R/usr/NX/etc
-
   # Persistent container storage for docker
   mkdir -p /var/lib/docker /tmp
   echo 'LABEL=linux /var/lib/docker btrfs nofail,subvol=containers 0 2' >> $R/etc/fstab
@@ -325,3 +274,49 @@ cd /run/initramfs/isoscan/config/updates/run
 find . -depth -type d -exec mkdir -p "/run/{}" \;
 find . -depth \! -type d -exec cp -a "{}" "/run/{}" \;
 cd -
+
+# first boot of a new instance, run all the “per-instance” configuration
+
+# Warning: error in this file will likely lead to failure to boot
+
+# Idea: detect if cloud-init is installed in the image and run the script during later in the boot phase
+# Any of thid is needed when a container is initialized ?
+# For LXC-like containers, ssh keys are needed
+# also - thsi is a useful way to run containers - https://medium.com/swlh/docker-and-systemd-381dfd7e4628
+
+# Consider only testing/changing this file in a development environment with access to console
+
+# This script logs to /run/initramfs/rdexec.out
+# Determine where this script is called from - either as a dracut hook or as a cloud-init phase or 'docker run'
+
+# This script is optional for containers, that are not "booted" and do not initialize systemd. Keep it that way
+
+# cloud-init does similar things
+
+# Do not change /usr to allow ro mount
+# Ideally only make changes in /var
+# To avoid manipulating /etc use kernel arguments when possible
+# Instead of changing existing files, try to just add new files
+# Keep changing in /run
+
+# Configuration 1 - OverlayRoot
+# Rootfs is mounted ro and EFI partition is mounted as ro to customize rootfs overlay
+# /run can be used to create new files and directories that are not persistent between boots
+# Symlink /etc to /run when possible - use relative links. This will allow editing these files even if etc itself is mounted read-only
+
+# Input:
+# - environment variables, /proc filesystem, including /proc/cmdline
+# - current directory is set to where the rd.exec file is located
+# - $mp points to EFI partition root
+# - $NEWROOT points to root filesystem
+# - all changes to root filesystem are made in ram (overlayroot)
+
+# Autodetect the environment
+# Disks - /dev/disks, /proc/partitions
+# HW - /proc/cpuinfo
+# Grub - /proc/cmdline
+# Network - DHCP
+# rootfs version
+
+# TODO
+# move enabling/disabling systemd services out of this file into a kernel command line
