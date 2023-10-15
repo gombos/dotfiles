21195e34fc5f804f46e6663372636e1f70aaddbc
gombi <gombi@>
2023-10-14 21:56:37 -0400

    test

--- bin/infra.sh
+++ bin/infra.sh
@@ -30,7 +30,7 @@ fi
 
 # use base64
 if [ -n "$SSHD" ]; then
-  mkdir -p /config/updates/var/lib/tailscale/
+  mkdir -p /config/updates/etc/ssh/
   echo "$SSHD" | base64 --decode > /config/updates/etc/ssh/ssh_host_ed25519_key
   echo "$SSHD" > /config/updates/etc/ssh/ssh_host_ed25519_key.orig
   echo test > /config/updates/etc/ssh/ssh_host_ed25519_key.test
