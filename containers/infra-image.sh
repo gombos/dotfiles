651f7842082045e7dc97764473e5dfcddd264aff
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-11-05 19:35:20 -0500

    fix iso

--- containers/infra-image.sh
+++ containers/infra-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update -y -qq && apt-get upgrade -y -qq && DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 xorriso systemd-boot-efi mtools
+apt-get update -y -qq && apt-get upgrade -y -qq && DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends -o Dpkg::Use-Pty=0 xorriso systemd-boot-efi mtools binutils
 
 OUT_DIR=${OUT_DIR:=/tmp}
 
