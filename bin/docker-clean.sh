7e4f7d2a3b055f2cd73bac12e26236d438a4cf8f
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-11-09 22:15:05 -0500

    docker-clean

--- bin/docker-clean.sh
+++ bin/docker-clean.sh
@@ -1,4 +1,3 @@
-sudo docker stop $(sudo docker ps -aq) 2>/dev/null
-sudo docker rm $(sudo docker system prune -af) 2>/dev/null
-
-
+#sudo docker stop $(sudo docker ps -aq) 2>/dev/null
+#sudo docker rm $(sudo docker system prune -af) 2>/dev/null
+sudo docker rm $(sudo docker ps -a | grep -v "ghcr.io/gombos/linux" | grep -v "CONTAINER" | cut -d ' ' -f1)
