9e7231ad44b97595cbcafa02c76ae3700eaba346
gombi <gombi@>
2023-12-10 22:11:54 -0500

    comment

--- bin/infra-reset.sh
+++ bin/infra-reset.sh
@@ -1,3 +1,4 @@
 docker pull ghcr.io/gombos/linux:latest
 distrobox rm --force linux
 distrobox create --name linux --image ghcr.io/gombos/linux:latest --volume /run:/run  --volume /home:/home -Y --pre-init-hooks "sudo hostname $(uname -n)"
+#colima prune -f && colima delete -f && colima start
