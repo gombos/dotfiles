3b964d635d9c3d651afb6f27e826021b1ef39b3c
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-12-11 20:54:59 -0500

    change one thing at a time

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -63,15 +63,15 @@ printf "APT::Install-Recommends false;\nAPT::Install-Suggests false;\n" > etc/ap
 echo "deb https://deb.debian.org/debian ${RELEASE} main" > etc/apt/sources.list
 echo "deb https://security.debian.org/debian-security stable-security/updates main" >> etc/apt/sources.list
 
-# docker-ce
-packages_update_db.sh
-pi ca-certificates curl gnupg
-
-install -m 0755 -d etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o etc/apt/keyrings/docker.gpg
-chmod a+r etc/apt/keyrings/docker.gpg
-#echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. etc/os-release && echo "$RELEASE") stable" | tee etc/apt/sources.list.d/docker.list > /dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. etc/os-release && echo "$RELEASE") stable" | tee etc/apt/sources.list.d/docker.list > /dev/null
+## docker-ce
+#packages_update_db.sh
+#pi ca-certificates curl gnupg
+#
+#install -m 0755 -d etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o etc/apt/keyrings/docker.gpg
+#chmod a+r etc/apt/keyrings/docker.gpg
+##echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. etc/os-release && echo "$RELEASE") stable" | tee etc/apt/sources.list.d/docker.list > /dev/null
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. etc/os-release && echo "$RELEASE") stable" | tee etc/apt/sources.list.d/docker.list > /dev/null
 
 packages_update_db.sh
 packages_upgrade.sh
