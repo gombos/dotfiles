61b5dc4520eed696c84661370c378d5a4b3034a6
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-12-31 20:51:59 -0500

    perso

--- bin/clear-gh-actions.sh
+++ bin/clear-gh-actions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #user=LaszloGombos repo=dracut;
-user=dracutdevs-extra repo=dracut-canary;
-#user=gombos repo=dotfiles
+#user=dracutdevs-extra repo=dracut-canary;
+user=gombos repo=dotfiles
 #user=initramfs-modules repo=initramfs-modules;
 gh api repos/$user/$repo/actions/runs --paginate -q '.workflow_runs[] | "\(.id)"' | xargs -n1 -I % gh api repos/$user/$repo/actions/runs/% -X DELETE
