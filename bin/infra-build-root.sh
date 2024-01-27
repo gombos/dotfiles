4c286f1df2cf2d8b8f5209d336b81646e2488b43
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-01-27 17:33:21 -0500

    debug

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -92,11 +92,12 @@ cd bin && rm -rf sh && ln -s bash sh && cd -
 
 rm var/lib/dpkg/info/dash.pos* var/lib/dpkg/info/dash.pre*
 rm var/lib/dpkg/info/debianutils.pos* var/lib/dpkg/info/debianutils.pre*
+rm var/lib/dpkg/info/apt.pos* var/lib/dpkg/info/apt.pre*
 #dpkg --remove --force-remove-essential dash
 #apt-get remove -y --allow-remove-essential dash
 #apt-get remove -y --allow-remove-essential apt
 #apt-get install debianutils
-dpkg -P --force-remove-essential --force-all debianutils
+dpkg -P --force-remove-essential --force-all debianutils dash apt
 
 cd bin && rm -rf sh && ln -s bash sh && cd -
 
