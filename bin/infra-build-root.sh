8db86191cb8b7d957a05cc6b68ac6c4cb396e35d
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-01-27 09:49:51 -0500

    make default shell bash

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -88,7 +88,10 @@ install_my_packages.sh packages-boot.l packages-core.l
 # tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
-apt-get purge -y --allow-remove-essential  dash
+echo "dash dash/sh boolean false" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
+
+#apt-get purge -y --allow-remove-essential  dash
 
 # log installed packages
 dpkg -l
