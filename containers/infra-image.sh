8c0396d494ec95b9ea3fecc568249868f93d6432
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-02-01 16:46:06 -0500

    debug

--- containers/infra-image.sh
+++ containers/infra-image.sh
@@ -71,13 +71,16 @@ xorriso \
 
 
 # make unified kernel
-echo "rd.live.overlay.overlayfs=1 root=live:/dev/disk/by-label/ISO" > /tmp/cmdline
+echo "root=/dev/sr0" > /tmp/cmdline
 objcopy --verbose  \
     --add-section .osrel="/etc/os-release" --change-section-vma .osrel=0x20000 \
     --add-section .cmdline="/tmp/cmdline" --change-section-vma .cmdline=0x30000 \
     --add-section .linux="/tmp/iso/kernel/vmlinuz" --change-section-vma .linux=0x40000 \
-    --add-section .initrd="/tmp/iso/kernel/initrd.img" --change-section-vma .initrd=0x3000000 \
     /usr/lib/systemd/boot/efi/linuxx64.efi.stub /tmp/iso/EFI/BOOT/BOOTX64.efi
+
+#   --add-section .initrd="/tmp/iso/kernel/initrd.img" --change-section-vma .initrd=0x3000000 \
+# 184418304
+# 176M
 
 ## experiment for minimal iso
 rm -rf extensions
