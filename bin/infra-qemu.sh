65fc2889e24fed1abcb447dda102031d5022c74d
gombi <gombi@>
2024-02-05 15:17:07 -0500

    baseline

--- bin/infra-qemu.sh
+++ bin/infra-qemu.sh
@@ -26,6 +26,28 @@ fi
 # todo
 #switch to UEFI boot with full iso as test case
 f=$(ls /opt/homebrew/Cellar/qemu/*/share/qemu/edk2-x86_64-code.fd)
+
+#qemu-system-aarch64 --cdrom $ISO -cpu cortex-a57 -M virt,highmem=off -monitor stdio -vga none
+
+#!/bin/sh
+#qemu-system-aarch64 \
+#    -accel hvf \
+#    -m 2048 \
+#    -cpu cortex-a57 -M virt,highmem=off  \
+#    -drive file=/usr/local/share/qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on \
+#    -drive file=ovmf_vars.fd,if=pflash,format=raw \
+#    -serial telnet::4444,server,nowait \
+#    -drive if=none,file=disk.qcow2,format=qcow2,id=hd0 \
+#    -device virtio-blk-device,drive=hd0,serial="dummyserial" \
+#    -device virtio-net-device,netdev=net0 \
+#    -netdev user,id=net0 \
+#    -vga none -device ramfb \
+#    -cdrom ubuntu-20.04.2-live-server-arm64.iso \
+#    -device usb-ehci -device usb-kbd -device usb-mouse -usb \
+#    -monitor stdio
+
+
+
 qemu-system-x86_64 -m 1024 -nographic -drive file=fat:rw:~/.dotfiles/boot,format=vvfat,label=CONFIG --cdrom $ISO -global driver=cfi.pflash01,property=secure,value=on  -drive if=pflash,format=raw,unit=0,file="$f"
 #-drive if=pflash,format=raw,unit=0,file="/opt/homebrew/Cellar/qemu/8.2.0/share/qemu/edk2-x86_64-code.fd",readonly=on -smbios type=11,value=io.systemd.stub.kernel-cmdline-extra="$1"
 
