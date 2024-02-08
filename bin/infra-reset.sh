9fec16503d1502350b1a72dd0697bcbd1797e982
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-02-08 13:33:43 +0000

    sid

--- bin/infra-reset.sh
+++ bin/infra-reset.sh
@@ -1,4 +1,7 @@
+docker system prune -a -f
 docker pull ghcr.io/gombos/linux:latest
 distrobox rm --force linux
-distrobox create --name linux --image ghcr.io/gombos/linux:latest --volume /run:/run  --volume /home:/home -Y --pre-init-hooks "sudo hostname $(uname -n)"
+distrobox create --name linux --image ghcr.io/gombos/linux:latest --volume /run:/run  --volume /home:/home -Y
+distrobox enter linux
+# --pre-init-hooks "sudo hostname $(uname -n)"
 #colima prune -f && colima delete -f && colima start
