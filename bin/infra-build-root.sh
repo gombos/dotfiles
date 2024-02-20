2d3e271632cda8e85e741bfc11e6b37ab69066cf
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-02-19 19:40:31 -0500

    simplify

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -137,9 +137,6 @@ mkdir -m 0755 -p /etc/apt/keyrings /etc/apt/sources.list.d
 curl -fsSL https://dl.google.com/linux/linux_signing_key.pub > /etc/apt/keyrings/google.asc
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/google.asc] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
-ls -la /etc/apt/sources.list.d/
-cat /etc/apt/sources.list.d/*
-
 packages_update_db.sh
 packages_upgrade.sh
 
