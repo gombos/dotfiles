3d6770f7f11ba267903e25e1076551d1cd582380
gombi <gombi@>
2024-02-23 07:37:50 -0500

    do not nuke all containers

--- bin/infra-reset.sh
+++ bin/infra-reset.sh
@@ -1,8 +1,8 @@
 # stop all containers
-docker ps -aq | xargs docker stop | xargs docker rm
+#docker ps -aq | xargs docker stop | xargs docker rm
 
 # prune all container images
-docker system prune -a -f
+#docker system prune -a -f
 
 # pull in latest container
 docker pull ghcr.io/gombos/linux:latest
