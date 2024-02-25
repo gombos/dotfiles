06036264441f551fce5d1da835b35c8f5313adb2
gombi <gombi@>
2024-02-24 20:40:31 -0500

    simplify

--- bin/infra-reset.sh
+++ bin/infra-reset.sh
@@ -11,7 +11,8 @@ docker pull ghcr.io/gombos/linux:latest
 
 # reinitialize distrobox
 distrobox rm --force linux
-distrobox create --name linux --image ghcr.io/gombos/linux:latest --volume /run:/run  --volume /home:/home -Y
+# distrobox create
+# --name linux --image ghcr.io/gombos/linux:latest --volume /run:/run  --volume /home:/home -Y
 distrobox enter linux
 # --pre-init-hooks "sudo hostname $(uname -n)"
 #colima prune -f && colima delete -f && colima start
