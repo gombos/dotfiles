b31264f5ecb7fa7728158bc491ee4a45d762c0d5
gombi <gombi@>
2024-03-23 11:15:28 -0400

    kill dpkg without killing its dependencies

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -119,8 +119,10 @@ rm -rf lib*32
 # debug
 dpkg -l
 
-#rm var/lib/dpkg/info/dpkg.*rm
-#apt-get purge -y --allow-remove-essential dpkg
+sed -ni '/Depends: dpkg/!p' var/lib/dpkg/status
+rm var/lib/dpkg/info/dpkg.*rm
+apt-get purge -y --allow-remove-essential dpkg
+
 # todo
 # this removes python and apparmor-utils which likely break running distrobox
 
