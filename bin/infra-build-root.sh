b81fc50add2fd2d4cb88f8803d0e7241aebee95d
gombi <gombi@>
2024-03-24 21:18:09 -0400

    kill passwd

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -91,7 +91,7 @@ sed -ni '/^PreDepends:/!p' /var/lib/dpkg/status
 apt-get purge -y runit-helper sensible-utils ucf
 
 # todo, fix upstream dependencies and make it recommends
-apt-get purge -y util-linux-extra dmsetup libdevmapper* adduser
+apt-get purge -y util-linux-extra dmsetup libdevmapper* adduser passwd
 
 # prefer gawk
 apt-get purge -y --allow-remove-essential mawk
