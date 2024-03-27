2ca24f4272576421b67d81779e158e27625d1a82
gombi <gombi@>
2024-03-26 21:43:42 -0400

    iptables

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -140,20 +140,20 @@ cd /usr/bin &&
   ln -fs netcat nc &&
 cd -
 
-#cd /usr/sbin &&
-#  ln -fs ip6tables-nft ip6tables &&
-#  ln -fs ip6tables-nft-save ip6tables-save &&
-#  ln -fs ip6tables-nft-restore ip6tables-restore &&
-#  ln -fs iptables-nft iptables &&
-#  ln -fs iptables-nft-save iptables-save &&
-#  ln -fs iptables-nft-restore iptables-restore &&
-#  ln -fs etables-nft etables &&
-#  ln -fs etables-nft-save etables-save &&
-#  ln -fs etables-nft-restore etables-restore &&
-#  ln -fs arptables-nft arptables &&
-#  ln -fs arptables-nft-save arptables-save &&
-#  ln -fs arptables-nft-restore arptables-restore &&
-#cd -
+cd /usr/sbin &&
+  ln -fs ip6tables-nft ip6tables &&
+  ln -fs ip6tables-nft-save ip6tables-save &&
+  ln -fs ip6tables-nft-restore ip6tables-restore &&
+  ln -fs iptables-nft iptables &&
+  ln -fs iptables-nft-save iptables-save &&
+  ln -fs iptables-nft-restore iptables-restore &&
+  ln -fs etables-nft etables &&
+  ln -fs etables-nft-save etables-save &&
+  ln -fs etables-nft-restore etables-restore &&
+  ln -fs arptables-nft arptables &&
+  ln -fs arptables-nft-save arptables-save &&
+  ln -fs arptables-nft-restore arptables-restore &&
+cd -
 
 find /var
 
