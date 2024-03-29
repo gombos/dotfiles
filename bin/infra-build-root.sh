1d5fff35427f0fc31a3810656cc94baac4d5e944
Laszlo Gombos <laszlo.gombos@gmail.com>
2024-03-29 09:02:39 -0400

    try to change order

--- bin/infra-build-root.sh
+++ bin/infra-build-root.sh
@@ -215,10 +215,9 @@ ls -la /usr/bin/
 
 if [ "$TARGET" = "container" ]; then
   apt install net-tools man file -y
-  #yes| unminimize
 
   install_my_packages.sh packages-container.l
-
+  yes| unminimize
 
   echo '%sudo ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/sudoers
 
