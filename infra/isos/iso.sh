9915693302279a2b2b7d1bffdf579cd2c209f5b1
gombi <gombi@>
2026-01-04 12:48:01 -0500

    s

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -3,3 +3,10 @@ wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-d
 wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/9.01/systemrescue-9.01-amd64.iso/download -O systemrescue.iso
 wget http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso -O KNOPPIX.iso
 wget https://github.com/gombos/dotfiles/releases/download/iso/linux.iso
+
+https://enterprise.proxmox.com/iso/proxmox-ve_9.1-1.iso
+https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/alpine-standard-3.23.2-x86_64.iso
+https://boot.netboot.xyz/ipxe/netboot.xyz.iso
+https://grml.org/download/
+http://www.tinycorelinux.net/
+https://www.finnix.org/
