dd28e8b94da47b2c20941a5d016115ff3972c90b
usr <usr@>
2026-01-19 17:53:14 -0500

    s

--- infra/isos/iso.sh
+++ infra/isos/iso.sh
@@ -1,12 +1,16 @@
-wget https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso -O Fedora-Workstation.iso
-wget https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso -O lubuntu.iso
-wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/9.01/systemrescue-9.01-amd64.iso/download -O systemrescue.iso
-wget http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso -O KNOPPIX.iso
-wget https://github.com/gombos/dotfiles/releases/download/iso/linux.iso
+https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso
+https://cdimage.ubuntu.com/lubuntu/releases/20.04/release/lubuntu-20.04.3-desktop-amd64.iso
 
+# best rescue, arch based
+
+https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/9.01/systemrescue-9.01-amd64.iso/download
+http://mirrors.sonic.net/knoppix/knoppix-dvd/KNOPPIX_V9.1DVD-2021-01-25-EN.iso -O KNOPPIX.iso
+https://github.com/gombos/dotfiles/releases/download/iso/linux.iso
 https://enterprise.proxmox.com/iso/proxmox-ve_9.1-1.iso
 https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/alpine-standard-3.23.2-x86_64.iso
 https://boot.netboot.xyz/ipxe/netboot.xyz.iso
 https://grml.org/download/
 http://www.tinycorelinux.net/
+
+# debian based rescue
 https://www.finnix.org/
